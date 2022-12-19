83d9c8bbb5a5b035e6949e25f3917da382d93bde
Riadh18 <allaniriadh103@gmail.com>
2022-12-18 14:32:59 -1100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(){
+    super()
+    this.state={
+      
+      tasks : [
+        {desc : "Onion", isDone : false, id : Math.random()},
+        {desc : "carrot", isDone : false, id : Math.random()},
+        {desc : "melon", isDone : false, id : Math.random()}
+      ],
+      textP : ''
+    }
+  }
+  handleAdd=()=>{
+    if (this.state.textP ==="") {
+      alert('Le champ est vide') 
+    } else {
+      this.setState({tasks : [...this.state.tasks,{desc : this.state.textP,isDone : false, id : Math.random()}]})
+      this.setState({textP : ''})
+    }
+  }
+  handleDelete=(a)=> this.setState({tasks : this.state.tasks.filter(el => el.id !== a)})
+  handleDone=(a)=> this.setState({tasks : this.state.tasks.map(el => el.id === a ? {...el,isDone : !el.isDone} : el)})
+  render(){
+    return(
+      <div>
+        <img className='pic1' src='/to.png' alt='Not Found'/>
+        <h1>My Tasks</h1>
+
+        {
+          this.state.tasks.map(el=> 
+            <div>
+              <h2 className={el.isDone && 'done'}>{el.desc}</h2>
+              
+              <i class="fa-solid fa-trash" onClick={()=>this.handleDelete(el.id)}></i>
+              <button className='bt' onClick={()=> this.handleDone(el.id)}>{el.isDone ? 'Undone' : "Done"}</button>
+              
+            </div>
+            )
+            
+            
+        }
+        <br/>
+        <input  value={this.state.textP} type="text" onChange={(e)=>this.setState({textP : e.target.value})}/>
+       
+        <i class="fa-solid fa-circle-plus" onClick={this.handleAdd}></i>
+        
+
+        
+      </div>
+    )
+  }
 }
 
 export default App;
